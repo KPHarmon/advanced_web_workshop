e4be1a4d758e4c7bb13a9cb712f257a4be2f91c5
KPHarmon <kade_harmon@yahoo.com>
2021-01-19 12:24:25 -0800

    Added comments

--- app.py
+++ app.py
@@ -4,32 +4,44 @@ from db import *
 # Initialize App
 app = Flask(__name__, static_folder='templates/static', static_url_path='')
 
-# SQL Login
+# SQL Login Page
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	msg = None
+
+	# If the user sents a POST request (i.e. they submit a form) run this logic
 	if request.method == 'POST':
+		
+		# Grab the username and password the user submitted via the form
 		user = request.form['username']
 		password = request.form['password']
 
+		# Function from db.py that creates a query searching for the username and password in the database
 		data = query_username(user, password)
+
+		# If the user left on of the fields blank, return incorrect password
 		if user == "" or password == "":
 			msg = ["Incorrect Password"]
 			resp = make_response(render_template('login.html', msg=msg))
 		
+		# If the query returns more than on item, return all of the items
 		if len(data) > 1:
 			msg = data
 			resp = make_response(render_template('login.html', msg=msg))
 
+		# If a user logs in with the correct username and password, redirect to the profile page
 		elif request.form['username'] == "crounds3" and request.form['password'] == "niBhfz33d5":
 			msg = request.form['username']
 			resp = redirect(url_for('profile'))
+
+		# If they log in with the wrong credentials, return an incorrect password
 		else:
 			msg = ["Incorrect Password"]
 			resp = make_response(render_template('login.html', msg=msg))
 		
 		return resp
 
+	If the user sends a POST request (i.e. they open the page without submitting a form) just return the webpage
 	if request.method == 'GET':
 		resp = make_response(render_template('login.html', msg=msg))
 		return resp
@@ -39,17 +51,24 @@ def profile():
 	
 	usr = 'crounds3'
 		
+	# If someone submits a comment, run this logic
 	if request.method == 'POST':
+
+		# Grab the comment from the form data and then create a query that inserts the comment into the database
 		post = request.form['comment']
 		data = query_posts(post)
 			
+		# If they submitted a blank comment, do nothing
 		if post == "":
 			msg = None
+
+		# If they submit a comment, return the output from the database
 		else:
 			msg = data
 		resp = make_response(render_template('profile.html', msg=msg, usr=usr))
 		return resp
 
+	# If they do not submit a form to the page, just open the page normally
 	elif request.method == 'GET':
 		msg = None
 	return render_template('profile.html', msg=msg, usr=usr)
--- db.py
+++ db.py
@@ -1,11 +1,14 @@
 import psycopg2
 
+# Helper functions to access the database
+
+# Function given postgres server details will return a connection
 def initialize_connection():
 
 	# Connection Variables
-	DB_NAME="test"
-	USER="test"
-	PASSWORD="password"
+	DB_NAME= "XXXXX"
+	USER="XXXXX"
+	PASSWORD="XXXXX"
 	HOST="127.0.0.1"
 	PORT="5432"
 
@@ -13,6 +16,7 @@ def initialize_connection():
 	conn = psycopg2.connect(database=DB_NAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
 	return conn
 
+# Helper function to destroy a table
 def destroy_database(table_name):
 	# Create Cursor
 	conn = initialize_connection()
@@ -23,7 +27,7 @@ def destroy_database(table_name):
 	conn.commit()
 	conn.close()
 	
-
+# Helper function to generate a table with dummy data (must have text file with a query)
 def generate_database(table_name):
 	conn = initialize_connection()
 	curs = conn.cursor()
@@ -36,7 +40,7 @@ def generate_database(table_name):
 	conn.commit()
 	conn.close()
 
-
+# Function that will create a query to check the database for login credentials
 def query_username(username, password):
 	
 	query = "SELECT * FROM users WHERE username='{}' AND password='{}';".format(username, password)
@@ -48,19 +52,7 @@ def query_username(username, password):
 
 	return rows
 
-
-
-def query_username(username, password):
-	
-	query = "SELECT * FROM users WHERE username='{}' AND password='{}';".format(username, password)
-	conn = initialize_connection()
-	cur = conn.cursor()
-	cur.execute(query)
-	rows = cur.fetchall()
-	conn.close()
-
-	return rows
-
+# Function that will INSERT INTO the "posts" table and return the output
 def query_posts(message):
 	
 	query = "INSERT INTO posts (message_id, time_posted, author, post) values (101, '01/14/2020', 'crounds3', '{}') RETURNING post;".format(message)
